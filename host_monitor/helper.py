c96f916f791654e7f7c8875462478ad9dfb3b04a
Richard Ma <richard.ma.19850509@gmail.com>
2017-05-07 20:24:19 +0800

    use upper case header name

--- host_monitor/helper.py
+++ host_monitor/helper.py
@@ -7,7 +7,8 @@ def get_client_ip(request):
     ip = request.remote_addr
 
     if on_heroku():
-        header = request.headers.get('x-forwarded-for')
-        ip = header.split(',')[-1]
+        header = request.headers.get('X-Forwarded-For')
+        ip = header
+        #ip = header.split(',')[-1]
 
     return ip
